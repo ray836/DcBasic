cf1580ff90dc3214e76c585f3005c8af5777c7e7
ray836 <raygrant36@gmail.com>
2023-05-11 21:28:42 -0600

    Initial commit

---
+++ test/dc_basic.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DcBasic from '../lib/dc_basic-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dc_basic-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DcBasic.DcBasicStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
