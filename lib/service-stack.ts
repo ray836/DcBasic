226a38522c9e98e512ca543308ed800eacd47e18
ray836 <raygrant36@gmail.com>
2023-05-22 11:06:16 -0600

    add lambdaRole

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { ManagedPolicy, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { CfnParametersCode, Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
@@ -10,11 +12,27 @@ export class ServiceStack extends Stack {
 
 		this.serviceCode = Code.fromCfnParameters()
 
+
+		const providerTable = new Table(this, 'ProviderTable', {
+			partitionKey: {name: 'id', type: AttributeType.STRING},
+			billingMode: BillingMode.PAY_PER_REQUEST,
+			tableName: 'Provider'
+		});
+
+		const lambdaARole = new Role(this, 'LambdaRole', {
+			assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+		  });
+
+		  lambdaARole.addManagedPolicy(
+			ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')
+		  );
+
 		const expressLambda = new Function(this, 'ServiceLambda', {
 			runtime: Runtime.NODEJS_18_X,
 			handler: 'src/lambda.handler',
 			code: this.serviceCode,
-			functionName: 'ServiceLambda'
+			functionName: 'ServiceLambda',
+			role: lambdaARole,
 		})
 
 		const api = new LambdaRestApi(this, 'DcBasicApi', {
